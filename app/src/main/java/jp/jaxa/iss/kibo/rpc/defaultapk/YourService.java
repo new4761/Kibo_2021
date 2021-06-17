c77746ab728161e0a9b49f010da7a61af4123b13
new4761 <brid47@outlook.com>
2021-06-17 20:07:35 +0700

    update qrcode

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,6 +1,7 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
 
+import android.graphics.Bitmap;
 import android.util.Log;
 
 import gov.nasa.arc.astrobee.Kinematics;
@@ -9,8 +10,15 @@ import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
+import org.opencv.core.CvException;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.objdetect.QRCodeDetector;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
@@ -18,10 +26,14 @@ import org.opencv.objdetect.QRCodeDetector;
 
 public class YourService extends KiboRpcService {
 
-    Point qr_Point = new Point(11.21,-9.8,4.79);
+    //Point qr_Point = new Point(11.21,-9.8,4.79);
+   Point qr_Point = new Point(11.353-0.02,-10.096-0.05,4.9837+0.05);
     Quaternion qr_Quaternion = new Quaternion(0,0,-0.707f,0.707f);
     Point point_b = new Point(10.6,-8.0,4.5);
     Quaternion quaternion_b = new Quaternion(0,0,-0.707f,0.707f);
+    String debugStr =  "MY DEBUG";
+    String errorStr =  "MY ERROR";
+    String ImagePath = "/sdcard/Pictures";
     @Override
 
     protected void runPlan1(){
@@ -29,12 +41,15 @@ public class YourService extends KiboRpcService {
        // api.getRobotKinematics();
        // api.getTrustedRobotKinematics();
         api.moveTo(qr_Point,qr_Quaternion,true);
-
-        //api.relativeMoveTo(qr_Point,qr_Quaternion,true);
+       // api.moveTo(newMove,qr_Quaternion,true);
+        //api.relativeMoveTo(qr_Point,qr_Quaternion,true);\
+        Log.i(debugStr,"takeQr");
         takeQr();
-        api.moveTo(point_b,quaternion_b,true);
+        //api.moveTo(point_b,quaternion_b,true);
         //api.relativeMoveTo(point_b,quaternion_b,true);
+        Log.i(debugStr,"takeSnapShot");
         takeSnapShot();
+        Log.i(debugStr,"reportMissionCompletion");
         api.reportMissionCompletion();
     }
 
@@ -53,19 +68,53 @@ public class YourService extends KiboRpcService {
         //this.gsService.sendData(MessageType.JSON, "data", data2.toString());
         api.flashlightControlFront(1.0F);
         Mat image = api.getMatNavCam();
+        //saveImage(image);
         api.flashlightControlFront(0.0F);
         QRCodeDetector qrcodeDetector = new QRCodeDetector();
         String content  = qrcodeDetector.detectAndDecode(image);
         Log.i("qrcodeDetector",content);
+        if(!content.isEmpty())
+        api.sendDiscoveredQR(content);
         Log.i("Mat getMatNavCam",image.toString());
 
     }
     void takeSnapShot(){
-        api.laserControl(true);
+        //api.laserControl(true);
         api.takeSnapshot();
-        api.laserControl(false);
+        //api.laserControl(false);
     }
+    void saveImage(Mat image){
+        //src = Imgcodecs.("test.jpg")
+       // Mat image = api.getMatNavCam();
+        Log.i(debugStr,"saveImage");
+       // File mydir = this.getFilesDir();
+        //File dataDir = this.getDataDir();
+        try {
 
+           // dataDir.toPath();
+         //   Log.i("mydir",mydir.toString());
+          //  Log.i("dataDir",dataDir.toString());
+            Imgcodecs.imwrite(ImagePath+"/Mat.png", image);
+            //Log.i("dataDir",dataDir.toString());
+        }
+        catch (CvException e){
+            Log.e(errorStr,e.toString());
+        }
+        try {
+            Log.i(debugStr,"BitMap");
+            Bitmap bitimage = api.getBitmapNavCam();
+            Log.i("BitMap getBitmapNavCam",bitimage.toString());
+            FileOutputStream out = new FileOutputStream(ImagePath+"/Bitmap.png");
+            bitimage.compress(Bitmap.CompressFormat.PNG,100,out);
+            out.flush();
+            out.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
 
